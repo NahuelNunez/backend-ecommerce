c70807f117f72452ddbd8bf97307544a5abaf561
NahuelNunez <lusknaru@gmail.com>
2025-07-31 15:32:31 -0300

    modify cloudinary

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,37 +1,37 @@
+import {v2 as cloudinary} from 'cloudinary';
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { cloudinary } from "../utils/cloudinary";
+
 import multer from "multer";
-import { Request } from "express";
-import { FileFilterCallback } from "multer";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    params: async(req:Request, file:Express.Multer.File) => ({
+    params: async(req, file) => ({
     
             folder:"uploads",
-            allowed_formats:"auto",
+            allowed_formats:["jpg", "jpeg", "png", "webp"],
             public_id: `${Date.now()}-${file.originalname}`,
         }),
     });
 
-    const fileFilter = (req:Request, file:Express.Multer.File, cb:FileFilterCallback) => {
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp', 'application/pdf'];
+ 
 
-        if (allowedTypes.includes(file.mimetype)) {
-            cb(null,true);
-        } else {
-            cb(new Error('Solo se permiten imagenes y PDF'))
-        }
-    };
+export const cloudinaryConfig = async () => {
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    console.log("✅ Configurate cloudinary");
+  } catch (error) {
+    throw new Error("❌ It is not possible to connect to Cloudinary");
+  }
+};
 
-
-    export const upload = multer({
-        storage,
-        fileFilter,
-      
-    })
-        
-
+export const upload = multer({ storage });
 
 
 
@@ -43,34 +43,3 @@ const storage = new CloudinaryStorage({
 
 
 
-
-// import multer, { FileFilterCallback } from 'multer';
-// import path from 'path';
-// import { Request } from 'express';
-
-// // Configuración del almacenamiento
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/');
-//     },
-//     filename: (req, file, cb) => {
-//         const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + path.extname(file.originalname);
-//         cb(null, uniqueName);
-//     }
-// });
-
-// // Filtro de archivos para aceptar solo imágenes
-// const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-//     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp','application/pdf'];
-//     if (allowedTypes.includes(file.mimetype)) {
-//         cb(null, true);
-//     } else {
-//         cb(new Error('Solo se permiten archivos de imagen (jpeg, png, jpg, webp)'));
-//     }
-// };
-
-// export const upload = multer({ 
-//     storage,
-//     fileFilter,
-//     limits: { fileSize: 5 * 1024 * 1024 } // 5MB máximo
-// });
