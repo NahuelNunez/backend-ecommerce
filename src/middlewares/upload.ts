1cde39e87991456cf5c6c801d4c0c4fdea003b6b
NahuelNunez <lusknaru@gmail.com>
2025-07-31 15:45:10 -0300

    added log cloudinary

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -6,6 +6,12 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
+
+console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API key:", process.env.CLOUDINARY_API_KEY);
+console.log("API secret:", process.env.CLOUDINARY_API_SECRET);
+
+
 const storage = new CloudinaryStorage({
     cloudinary,
     params: async(req, file) => ({
